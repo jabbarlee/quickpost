a5d3d5e298cd359f5d357afd0f43f00324dba806
“Amil <amiljabbarlee@gmail.com>
2024-10-11 11:55:35 -0500

    components created

--- src/app/(web)/create/recipients/[id]/page.tsx
+++ src/app/(web)/create/recipients/[id]/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import RecipientsForm from '@/components/pages/CreatePage/RecipientsForm'
 
 export default function page({ params }: { params: { id: string } } ) {
   return (
     <div>
-
+        <RecipientsForm id={params.id}/>
     </div>
   )
 }
--- src/components/pages/CreatePage/RecipientsForm/index.tsx
+++ src/components/pages/CreatePage/RecipientsForm/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import RecipientsFormTemplate from './RecipientsFormTemplate'
 
-export default function index() {
+export default function index( { id }: { id: string } ) {
   return (
     <div>
-        
+        <p>Add your email recipients</p>
+        <RecipientsFormTemplate id={id}/>
     </div>
   )
 }
